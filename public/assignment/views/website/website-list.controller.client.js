7636eb479c7c3a3986fbcc2b5a5a407c50630e8a
Kovit Nisar <kovit@ccs.neu.edu>
2016-05-26 10:47:15 -0400

    synin with professor github account, adding controllers and services for user, config routes

---
+++ public/assignment/views/website/website-list.controller.client.js
@@ -0,0 +1,15 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("WebsiteListController", WebsiteListController);
+
+    function WebsiteListController($routeParams, WebsiteService) {
+        var vm = this;
+
+        function init() {
+            var userId = $routeParams.userId;
+            vm.websites = WebsiteService.findWebsitesForUser(userId);
+        }
+        init();
+    }
+})();
