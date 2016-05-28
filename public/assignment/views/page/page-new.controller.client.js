1fc9dbb7658ff6f593c247cdedd5410425ab9a99
Kovit Nisar <kovit@ccs.neu.edu>
2016-05-28 09:57:11 -0400

    page list html and controller done

---
+++ public/assignment/views/page/page-new.controller.client.js
@@ -0,0 +1,22 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("NewWebsiteController", NewWebsiteController);
+
+    function NewWebsiteController($routeParams, WebsiteService, $location) {
+        var vm = this;
+        vm.userId = $routeParams.id;
+        vm.websiteId = $routeParams.wid;
+        vm.createWebsite = createWebsite;
+
+        function createWebsite() {
+            var result = WebsiteService.createWebsite(vm.userId, vm.website);
+
+            if(result) {
+                $location.url("/user/" + vm.userId + "/website");
+            } else {
+                vm.error = "Failed to create website"
+            }
+        }
+    }
+})();
