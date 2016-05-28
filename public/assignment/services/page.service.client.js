bac0093dc4d17933946b1962fa1f8a147d9b9c8c
Kovit Nisar <kovit@ccs.neu.edu>
2016-05-28 09:40:06 -0400

    page service, not tested

---
+++ public/assignment/services/page.service.client.js
@@ -0,0 +1,75 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .factory("PageService", PageService);
+
+    var pages = [
+        { "_id": "321", "name": "Post 1", "websiteId": "456" },
+        { "_id": "432", "name": "Post 2", "websiteId": "456" },
+        { "_id": "543", "name": "Post 3", "websiteId": "456" }
+    ];
+
+
+    function PageService() {
+        var api = {
+            createPage: createPage,
+            findPageByWebsiteId: findPageByWebsiteId,
+            deletePage: deletePage,
+            findPageById: findPageById,
+            updatePage: updatePage
+        };
+        return api;
+
+        function deletePage(pageId) {
+            for(var i in pages) {
+                if(pages[i]._id === pageId) {
+                    pages.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function createPage(websiteId, page) {
+            var newPage = {
+                _id: (new Date()).getTime()+"",
+                name: page.name,
+                description: page.description,
+                websiteId: websiteId
+            };
+            pages.push(newPage);
+            return newPage;
+        }
+
+        function findPageByWebsiteId(websiteId) {
+            var resultSet = [];
+            for(var i in pages) {
+                if(pages[i].websiteId === websiteId) {
+                    resultSet.push(pages[i]);
+                }
+            }
+            return resultSet;
+        }
+
+        function findPageById(pageId) {
+            for(var i in pages) {
+                if(pages[i]._id === pageId) {
+                    return pages[i];
+                }
+            }
+            return false;
+        }
+
+        function updatePage(pageID, newPage) {
+            var page = findPageById(pageID);
+            if (page) {
+                page.name = newPage.name;
+                page.description = newPage.description;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+})();
