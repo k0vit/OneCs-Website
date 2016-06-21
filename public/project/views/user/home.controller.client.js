7c1405f2b762bf5bb16a1569adffc58619061e5d
Kovit Nisar <kovit@ccs.neu.edu>
2016-06-21 01:57:51 -0400

    home page search design done

--- public/project/views/user/home.controller.client.js
+++ public/project/views/user/home.controller.client.js
@@ -3,10 +3,26 @@
         .module("OneCs")
         .controller("HomeController", HomeController);
 
-    function HomeController() {
+    function HomeController($interval) {
         var vm = this;
-        vm.isCollapsed = true;
+        var slideCount = 2;
+        var slides = [
+            'resources/2016_news_computerscience-883x432.jpg',
+            'resources/computer-science.jpg',
+            'resources/most_popular_twitter_topics.png'
+        ];
 
-        vm.currentImg = "resources/2016_news_computerscience-883x432.jpg";
+        function init() {
+            vm.isCollapsed = true;
+            vm.slide = slides[0];
+            $interval(changeSlide, 10000);
+        }
+
+        init();
+
+        function changeSlide() {
+            vm.slide = slides[(slideCount%3)];
+            slideCount++;
+        }
     }
 })();
